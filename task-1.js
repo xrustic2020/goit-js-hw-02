5c33a0403d5b78ac3bf7550a34f2a5d8fb2f8b74
Ruslan Tisanyk <ruslantisanykot@gmail.com>
2020-05-10 11:27:02 +0300

    task_1 completed

---
+++ task-1.js
@@ -0,0 +1,12 @@
+const logItems = function (array) {
+  for (let i = 1; i <= (array.length); i += 1) {
+    console.log(`${i} - ${array[i - 1]
+}`);
+  }
+};
+
+// Вызовы функции для проверки работоспособности твоей реализации.
+
+logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
