0da4772a2c74331e03ecb7a7b1876fc6552edf2d
Ruslan Tisanyk <ruslantisanykot@gmail.com>
2020-05-20 22:30:00 +0300

    correct with mentor assigned

--- all-but-6/task-1.js
+++ all-but-6/task-1.js
@@ -4,9 +4,8 @@ console.log("// Task 1");
 console.log("==============================");
 
 const logItems = function (array) {
-  for (let i = 1; i <= (array.length); i += 1) {
-    console.log(`${i} - ${array[i - 1]
-}`);
+  for (let i = 0; i < (array.length); i += 1) {
+    console.log(`${i + 1} - ${array[i]}`);
   }
 };
 
