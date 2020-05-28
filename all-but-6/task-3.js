f4744b73d066ccb947cfd035ae94ed90931317d6
Ruslan Tisanyk <ruslantisanykot@gmail.com>
2020-05-28 15:58:01 +0300

    correct task 3 with mentor recomendations

--- all-but-6/task-3.js
+++ all-but-6/task-3.js
@@ -6,18 +6,14 @@ console.log("==============================");
 const findLongestWord = function (string) {
   const arr = string.split(" ");
   let max = 0;
+  let longestWord;
   for (let i = 0; i < arr.length; i += 1) {
     if (arr[i].length > max) {
       max = arr[i].length;
+      longestWord = arr[i];
     }
   }
-  let longestWord;
-  for (let element of arr) {
-    if (element.length === max) {
-      longestWord = element;
-      break
-    }
-  }
+  
   return longestWord;
 };
 
@@ -29,4 +25,3 @@ console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); //
 console.log(findLongestWord("Google do a roll")); // 'Google'
 
 console.log(findLongestWord("May the force be with you")); // 'force'
-
